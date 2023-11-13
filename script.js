c350385e6f25297683b0428eb506144dd5bc2ed6
poetraramadana0 <144464142+poetraramadana0@users.noreply.github.com>
2023-11-13 19:16:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+//dark-light mode
+var icon = document.getElementById("iconld");
+
+icon.onclick = function() {
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.src = "assets/images/moon1.png";
+    }else{
+        icon.src = "assets/images/sun.png";
+    }
+}
+
+// scroll section active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset =  sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+// sticky navbar
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+//remove toggle icon and navbar when click navbar link (scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+//scroll reveal
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+//typed js loop
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer'],
+    typeSpeed: 100,
+    typeSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
